04d4d03607dda1f9c2c9b6a15d5c9a3ffa042743
Steven Roose <steven@stevenroose.org>
2019-10-06 23:12:46 +0100

    Code clean-up

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ extern crate num_rational;
 
 pub mod highest_averages;
 
+/// A trait for seat allocation algorithms.
 pub trait AllocateSeats {
-    /// Calculates the number of seats per party given a vector of the number
-    /// of votes per party.
-    fn allocate_seats(&self, nb_seats: usize, parties: Vec<usize>) -> Vec<usize>;
+	/// Calculates the number of seats per party given a vector of the number
+	/// of votes per party.
+	fn allocate_seats(&self, nb_seats: usize, parties: Vec<usize>) -> Vec<usize>;
 }
